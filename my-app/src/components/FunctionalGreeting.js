e31a778ddc45e957c1c795646b48bd1d71751ca1
Niall Caplis <niallcaplis@gmail.com>
2024-08-25 14:41:57 +0000

    add stateless FunctionalGreeting component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,12 @@
+// This import unrequired in latest version, but makes backwards compatible
+import React from "react";
+
+// STANDARD FUNCTION
+// function FunctionalGreeting() {
+//     return <h1>Hello from React.</h1>
+// }
+
+// or, ARROW FUNCTION
+const FunctionalGreeting = () => <h1>Hello from React.</h1>;
+
+export default FunctionalGreeting;
