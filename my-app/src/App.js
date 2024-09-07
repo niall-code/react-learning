f031734ccb323f6755797a960d4b029176922a64
Niall Caplis <niallcaplis@gmail.com>
2024-09-07 13:43:04 +0000

    componentDidUpdate examples

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
+import LifeCyclesCDU from './components/LifeCyclesCDU';
 
 function App() {
   return (
     <div className="App">
-      <LifeCyclesCDM />
+      <LifeCyclesCDU />
     </div>
   );
 }
