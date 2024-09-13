37ed470bd096f6a015b355df4554c43d1beced49
Niall Caplis <niallcaplis@gmail.com>
2024-09-13 12:42:46 +0000

    UncontrolledForm.js alternative

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
 
 function App() {
   return (
     <div className="App">
-      <ControlledForm />
+      <UncontrolledForm />
     </div>
   );
 }
