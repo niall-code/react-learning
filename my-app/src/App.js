44dce9af4af351c3ef6af6e02f2790242466adb4
Niall Caplis <niallcaplis@gmail.com>
2024-09-14 15:05:02 +0000

    useState for objects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
     <div className="App">
-      <UseStateWithArrays />
+      <UseStateWithObjects />
     </div>
   );
 }
