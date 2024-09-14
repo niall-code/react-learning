1de289e2436cb259c0af77064536646fc0bf0cfc
Niall Caplis <niallcaplis@gmail.com>
2024-09-14 15:48:07 +0000

    prevent unnecessary re-rendering

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    // "Side effect" of another hook function
     useEffect(() => {
         document.title = count;
-    });
+        // Array of the only properties to watch for changes
+    }, [count]);
     return (
         <div>
             <button onClick={
