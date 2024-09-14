56528986b12b7593903ccb34e8522b7d7377f34f
Niall Caplis <niallcaplis@gmail.com>
2024-09-14 14:42:54 +0000

    useState for arrays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithArrays from './components/UseStateWithArrays';
 
 function App() {
   return (
     <div className="App">
-      <ControlledFormHooks />
+      <UseStateWithArrays />
     </div>
   );
 }
